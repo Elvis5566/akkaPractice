014cc25f1640bbbe4260220fecb2fe98e0e1a86f
Elvis Lee <JohanLiebhart@gmail.com>
2017-01-31 20:12:27 +0800

    Practice become method

--- src/main/scala/zzz/akka/avionics/FlightAttendant.scala
+++ src/main/scala/zzz/akka/avionics/FlightAttendant.scala
@@ -1,6 +1,7 @@
 package zzz.akka.avionics
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef, Cancellable}
+
 import scala.concurrent.duration._
 
 // This trait allows us to create different Flight Attendants
@@ -16,6 +17,11 @@ object FlightAttendant {
   case class GetDrink(drinkname: String)
 
   case class Drink(drinkname: String)
+
+  case class Assist(passenger: ActorRef)
+  case object Busy_?
+  case object Yes
+  case object No
 
   // By default we will make attendants that respond
   // within 5 minutes
@@ -33,12 +39,64 @@ class FlightAttendant extends Actor {
   // scheduler below
   implicit val ec = context.dispatcher
 
-  def receive = {
-    case GetDrink(drinkname) =>
-      // We don't respond right away, but use the scheduler to
-      // ensure we do eventually
-      context.system.scheduler.scheduleOnce(
-        responseDuration, sender, Drink(drinkname))
+  // An internal message we can use to signal that drink
+  // delivery can take place
+  case class DeliverDrink(drink: Drink)
+
+  // Stores our timer, which is an instance of 'Cancellable'
+  var pendingDelivery: Option[Cancellable] = None
+
+  // Just makes scheduling a delivery a bit simpler
+  def scheduleDelivery(drinkname: String): Cancellable = {
+    context.system.scheduler.scheduleOnce(responseDuration,
+      self,
+      DeliverDrink(Drink(drinkname)))
   }
+
+  // If we have an injured passenger, then we need to
+  // immediately assist them, by giving them the 'secret'
+  // Magic Healing Potion that's available on all flights in
+  // and out of Xanadu
+  def assistInjuredPassenger: Receive = {
+    case Assist(passenger) =>
+      // It's an emergency... stop what we're doing and
+      // assist NOW
+      pendingDelivery foreach { _.cancel() }
+      pendingDelivery = None
+      passenger ! Drink("Magic Healing Potion")
+  }
+
+  def handleDrinkRequests: Receive = {
+    case GetDrink(drinkname) =>
+      pendingDelivery = Some(scheduleDelivery(drinkname))
+      // Become something new
+      context.become(assistInjuredPassenger orElse handleSpecificPerson(sender))
+    case Busy_? =>
+      sender ! No
+  }
+
+  def handleSpecificPerson(person: ActorRef): Receive = {
+    // If the person asking us for a drink is the same
+    // person we're currently handling then we'll cancel
+    // what we were doing and service their new request
+    case GetDrink(drinkname) if sender == person =>
+      pendingDelivery foreach { _.cancel() }
+      pendingDelivery = Some(scheduleDelivery(drinkname))
+    case DeliverDrink(drink) =>
+      person ! drink
+      pendingDelivery = None
+      // Become something new
+      context.become(assistInjuredPassenger orElse
+        handleDrinkRequests)
+    // If we get another drink request when we're already
+    // handling one then we punt that back to our parent
+    // (the LeadFlightAttendant)
+    case m: GetDrink =>
+      context.parent forward m
+    case Busy_? =>
+      sender ! Yes
+  }
+
+  def receive = assistInjuredPassenger orElse handleDrinkRequests
 }
 
