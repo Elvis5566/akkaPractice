d13b76705cb8c2461a86dc9e7b0d77e8b6124ea4
Elvis Lee <JohanLiebhart@gmail.com>
2017-01-31 22:45:00 +0800

    Add HeadingIndicator and make ControlSurfaces can only access by Plane

--- src/main/scala/zzz/akka/avionics/ControlSurfaces.scala
+++ src/main/scala/zzz/akka/avionics/ControlSurfaces.scala
@@ -13,23 +13,47 @@ object ControlSurfaces {
 
   case class StickForward(amount: Float)
 
+  case class StickLeft(amount: Float)
+  case class StickRight(amount: Float)
+  case class HasControl(somePilot: ActorRef)
 }
 
 // Pass in the Altimeter as an ActorRef so that we can send
 // messages to it
-class ControlSurfaces(altimeter: ActorRef) extends Actor {
+class ControlSurfaces(plane: ActorRef, altimeter: ActorRef,
+                      heading: ActorRef) extends Actor {
 
   import ControlSurfaces._
   import Altimeter._
+  import HeadingIndicator._
 
-  def receive = {
-    // Pilot pulled the stick back by a certain amount,
-    // and we inform the Altimeter that we're climbing
-    case StickBack(amount) =>
+  def receive = controlledBy(context.system.deadLetters)
+
+  def controlledBy(somePilot: ActorRef): Receive = {
+    case StickBack(amount) if sender == somePilot =>
       altimeter ! RateChange(amount)
-    // Pilot pushes the stick forward and we inform the
-    // Altimeter that we're descending
-    case StickForward(amount) =>
-      altimeter ! RateChange(-amount)
+    case StickForward(amount) if sender == somePilot =>
+      altimeter ! RateChange(-1 * amount)
+    case StickLeft(amount) if sender == somePilot =>
+      heading ! BankChange(-1 * amount)
+    case StickRight(amount) if sender == somePilot =>
+      heading ! BankChange(amount)
+    // Only the plane can tell us who's currently in control
+    case HasControl(entity) if sender == plane =>
+      // Become a new instance, where the entity, which the
+      // plane told us about, is now the entity that
+      // controls the plane
+      context.become(controlledBy(entity))
   }
+
+//  def receive = {
+//    // Pilot pulled the stick back by a certain amount,
+//    // and we inform the Altimeter that we're climbing
+//    case StickBack(amount) =>
+//      altimeter ! RateChange(amount)
+//    // Pilot pushes the stick forward and we inform the
+//    // Altimeter that we're descending
+//    case StickForward(amount) =>
+//      altimeter ! RateChange(-amount)
+//  }
 }
